c178ec7666a24e880f3ead1a76674a8244e9ccd1
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2023-11-26 21:35:15 +0100

    favicon&title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = ({handleLogout}) => {
   return (
-    <div className='h-[66px] bg-messBlue flex items-center justify-between px-[5%] text-white ]'>
+    <div className='h-[66px] bg-messBlue flex items-center justify-between px-4 text-white ]'>
         <h1 className='font-bold text-[22px] font-logo'>Chatly</h1>
           <button onClick={handleLogout} className="bg-red-500 p-2 rounded-full hover:bg-red-700">
           Logout
